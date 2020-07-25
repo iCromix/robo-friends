3f628882ddc4bccb442a0677d293bbb6abb43be9
iCromix <gustavoscilingo1999@gmail.com>
2020-07-25 13:24:25 -0300

    Updates

---
+++ precache-manifest.030915fd3160fe3bd4da92372557b3e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d091f9aed77376aff019dc86163671b3",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "72bb8788f1e5040a0649",
+    "url": "/robo-friends/static/css/main.fd151650.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo-friends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72bb8788f1e5040a0649",
+    "url": "/robo-friends/static/js/main.915052fe.chunk.js"
+  },
+  {
+    "revision": "b7276070548a11fb737e",
+    "url": "/robo-friends/static/js/runtime-main.17f36485.js"
+  }
+]);
