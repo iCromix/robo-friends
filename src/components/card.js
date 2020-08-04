f3c059c2078ca58b875e097af2ab77f5048aa6ac
Gustavo <gustavoscilingo1999@gmail.com>
2020-08-04 11:03:32 -0300

    Added styles to the App

--- src/components/card.js
+++ src/components/card.js
@@ -5,8 +5,8 @@ const Card = ( { name, email, id } ) => {
         <div className='tc bg-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-            <h2>{name}</h2>
-            <p>{email}</p>
+            <h2 className='rnames'>{name}</h2>
+            <p className='rnames'>{email}</p>
             </div>
         </div>
     )
